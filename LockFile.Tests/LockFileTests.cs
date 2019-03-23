171dc8f24eb69ba937e4b30d67b72534579d2e25
Alexey Golub <Tyrrrrrr@gmail.com>
2019-03-23 14:36:53 +0200

    Add project files.

---
+++ LockFile.Tests/LockFileTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LockFile.Tests
+{
+    [TestFixture]
+    public class LockFileTests
+    {
+        public string TestDirPath => TestContext.CurrentContext.TestDirectory;
+
+        public string TempDirPath => Path.Combine(TestDirPath, "Temp");
+
+        [SetUp]
+        public void SetUp()
+        {
+            Directory.CreateDirectory(TempDirPath);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (Directory.Exists(TempDirPath))
+                Directory.Delete(TempDirPath, true);
+        }
+
+        [Test]
+        public void LockFileInfo_TryAcquire_Test()
+        {
+            var lockFilePath = Path.Combine(TempDirPath, Guid.NewGuid().ToString());
+
+            using (var lockFile = LockFile.TryAcquire(lockFilePath))
+            {
+                Assert.That(lockFile, Is.Not.Null);
+            }
+        }
+
+        [Test]
+        public void LockFileInfo_TryAcquire_AlreadyAcquired_Test()
+        {
+            var lockFilePath = Path.Combine(TempDirPath, Guid.NewGuid().ToString());
+
+            using (LockFile.TryAcquire(lockFilePath))
+            {
+                var lockFile = LockFile.TryAcquire(lockFilePath);
+                Assert.That(lockFile, Is.Null);
+            }
+        }
+
+        [Test]
+        public void LockFileInfo_WaitAcquire_Test()
+        {
+            var lockFilePath = Path.Combine(TempDirPath, Guid.NewGuid().ToString());
+
+            using (var originalLockFile = LockFile.TryAcquire(lockFilePath))
+            {
+                Task.Delay(TimeSpan.FromSeconds(0.5)).ContinueWith(_ => originalLockFile.Dispose());
+
+                using (var newLockFile = LockFile.WaitAcquire(lockFilePath))
+                    Assert.That(newLockFile, Is.Not.Null);
+            }
+        }
+
+        [Test]
+        public void LockFileInfo_WaitAcquire_Cancellation_Test()
+        {
+            var lockFilePath = Path.Combine(TempDirPath, Guid.NewGuid().ToString());
+
+            using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(0.5)))
+            using (LockFile.TryAcquire(lockFilePath))
+            {
+                Assert.Throws<OperationCanceledException>(() =>
+                    LockFile.WaitAcquire(lockFilePath, cts.Token));
+            }
+        }
+    }
+}
---
+++ LockFile/LockFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace LockFile
+{
+    public partial class LockFile : IDisposable
+    {
+        public FileStream FileStream { get; }
+
+        public LockFile(FileStream fileStream)
+        {
+            FileStream = fileStream;
+        }
+
+        public void Dispose() => FileStream.Dispose();
+    }
+
+    public partial class LockFile
+    {
+        public static LockFile TryAcquire(string filePath)
+        {
+            try
+            {
+                var fileStream = File.Open(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+                return new LockFile(fileStream);
+            }
+            // When access to file is denied, an IOException (not derived) is thrown
+            catch (IOException ex) when (ex.GetType() == typeof(IOException))
+            {
+                return null;
+            }
+        }
+
+        public static LockFile WaitAcquire(string filePath,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            while (true)
+            {
+                // Throw if canceled
+                cancellationToken.ThrowIfCancellationRequested();
+
+                // Try to acquire lock file
+                var lockFile = TryAcquire(filePath);
+                if (lockFile != null)
+                    return lockFile;
+            }
+        }
+    }
+}
